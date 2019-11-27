9561f56be7080af967019b74ecb69421715215d2
Shajitha Banu Sadiq <shajitha.sadiq@arukona.com>
2019-11-27 15:01:27 +0530

    5.244 fixes

--- zenspace_iot_gateway/settings.py
+++ zenspace_iot_gateway/settings.py
@@ -1,13 +1,13 @@
 APP_NAME='zenspace_iot_gateway'
-# ZEN_URL='https://zenspace-production.herokuapp.com/api/iot/reservation/retrieve/'
-# ZEN_STATE_URL='https://zenspace-production.herokuapp.com/api/iot/pod/save-state/'
-# ZEN_LOGICALNAME_URL="https://zenspace-production.herokuapp.com/api/iot/pod/retrieve/"
-# HOTSPOT_URL='https://zenspace-production.herokuapp.com/api/iot/pod/accesspoint/access/'
+ZEN_URL='https://zenspace-production.herokuapp.com/api/iot/reservation/retrieve/'
+ZEN_STATE_URL='https://zenspace-production.herokuapp.com/api/iot/pod/save-state/'
+ZEN_LOGICALNAME_URL="https://zenspace-production.herokuapp.com/api/iot/pod/retrieve/"
+HOTSPOT_URL='https://zenspace-production.herokuapp.com/api/iot/pod/accesspoint/access/'
 
-ZEN_URL='https://zensapce-staging.herokuapp.com/api/iot/reservation/retrieve/'
-ZEN_STATE_URL='https://zensapce-staging.herokuapp.com/api/iot/pod/save-state/'
-ZEN_LOGICALNAME_URL="https://zensapce-staging.herokuapp.com/api/iot/pod/retrieve/"
-HOTSPOT_URL='https://zensapce-staging.herokuapp.com/api/iot/pod/accesspoint/access/'
+# ZEN_URL='https://zensapce-staging.herokuapp.com/api/iot/reservation/retrieve/'
+# ZEN_STATE_URL='https://zensapce-staging.herokuapp.com/api/iot/pod/save-state/'
+# ZEN_LOGICALNAME_URL="https://zensapce-staging.herokuapp.com/api/iot/pod/retrieve/"
+# HOTSPOT_URL='https://zensapce-staging.herokuapp.com/api/iot/pod/accesspoint/access/'
 
 HOTSPOT_CLIENTS=15
 HOTSPOT_METHOD="local"
